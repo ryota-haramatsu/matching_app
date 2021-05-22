107b5cbf69cfc084d8624a50bc772cb571c57db9
ryota <haramatsuryota@gmail.com>
2021-05-22 19:04:11 +0900

    add: laravel api

--- README.md
+++ README.md
@@ -2,12 +2,36 @@
 
 docker-compose exec app bash
 
-docker compose up -d
+docker-compose up -d / docker-compose restart
 
-docker compose exec app bash
+docker-compose exec app bash
 
 composer create-project --prefer-dist "laravel/laravel=6.*" .
 
 npm install -D vue
 
 npm install -D vue-template-compiler
+
+composer require laravel/ui:^1.0 --dev
+
+php artisan ui vue
+
+npm install && npm run dev
+
+npm install --save vue-router
+
+npm run dev / npm run watch
+
+php artisan serve --host 0.0.0.0 --port 80
+
+php artisan make:migration create_tasks_table
+
+php artisan make:model Task
+
+php artisan make:seeder TasksTableSeeder
+
+php artisan migrate --seed
+
+mysql -u root -p -h 127.0.0.1 -P 3306
+
+docker-compose exec db bash
--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,48 @@
+import VueRouter from 'vue-router';
+import HeaderComponent from "./components/HeaderComponent";
+import TaskListComponent from "./components/TaskListComponent";
+import TaskShowComponent from "./components/TaskShowComponent";
+import TaskCreateComponent from "./components/TaskCreateComponent";
+import TaskEditComponent from "./components/TaskEditComponent";
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/tasks',
+            name: 'task.list',
+            component: TaskListComponent
+        },
+        {
+            path: '/tasks/create',
+            name: 'task.create',
+            component: TaskCreateComponent
+        },
+        {
+            path: '/tasks/:taskId',
+            name: 'task.show',
+            component: TaskShowComponent,
+            props: true
+
+        },
+        {
+            path: '/tasks/:taskId/edit',
+            name: 'task.edit',
+            component: TaskEditComponent,
+            props: true
+        },
+    ]
+});
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('header-component', HeaderComponent);
+
+const app = new Vue({
+    el: '#app',
+    router
+});
